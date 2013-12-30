4269b840d62ca6a9c096ff87a6324e942b1a5d7a
Rohan Poddar <rohan.poddar@gmail.com>
2013-12-30 14:02:10 +0530

    Initial Project Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImmigrationMgmt::Application.config.secret_token = '2326ccf52ced454e84cebadf9a39439a0c3ee782606908ce5d2cb0d9fa5515861ed8b31ba8165312c9af73f14289b13fb5b6e906294ac36f6821fffe7563e368'
