9407730575a36a67275320aab84982470aeb7ec7
Rohan Poddar <rohan.poddar@gmail.com>
2013-12-31 12:21:38 +0530

    Created Basic Employee controller and index view.

--- app/models/visa.rb
+++ app/models/visa.rb
@@ -1,5 +1,5 @@
 class Visa < ActiveRecord::Base
-  attr_accessible :epiry_date, :issue_date, :visa_id
+  attr_accessible :expiry_date, :issue_date, :visa_id
   belongs_to :visa, :foreign_key => 'passport_id'
   belongs_to :visa_type, :foreign_key => 'visa_type_id'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ImmigrationMgmt::Application.routes.draw do
+
+  resources :employees
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
