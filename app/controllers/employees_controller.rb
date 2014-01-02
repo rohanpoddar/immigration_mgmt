5861e46dbb2b44dc701d6ad7c4ccec98f447a3be
Rohan Poddar <rohan.poddar@gmail.com>
2014-01-02 13:01:10 +0530

    Completed CRUD functionalities for Employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,18 +4,32 @@ class EmployeesController < ApplicationController
   end
 
   def new
+    @employee = Employee.new
+  end
 
+  def create
+    @employee=Employee.new(params[:employee])
+    @employee.save
+    redirect_to :action => 'index'
   end
 
   def show
-
+    @employee=Employee.find(params[:id])
   end
 
   def destroy
-
+    @employee=Employee.find(params[:id])
+    @employee.destroy
+    redirect_to :action => 'index'
   end
 
   def edit
+    @employee=Employee.find(params[:id])
+  end
 
+  def update
+    @employee=Employee.find(params[:id])
+    @employee.update_attributes(params[:employee])
+    redirect_to :action => 'index'
   end
 end
