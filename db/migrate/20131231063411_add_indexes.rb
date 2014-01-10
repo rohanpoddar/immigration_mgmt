a0d3b03a36185a9c15241939c82f01336f14dadc
abhinandan24 <meghaagr@thoughtworks.com>
2014-01-10 11:25:30 +0530

    removed redundant indexes

---
+++ db/migrate/20131231063411_add_indexes.rb
@@ -0,0 +1,8 @@
+class AddIndexes < ActiveRecord::Migration
+  def change
+    #add_index :visas, :visa_type
+    add_index :immigrations, :city
+    add_index :visa_types, :country
+    #add_index :visa_types, :type, :unique => true
+  end
+end
