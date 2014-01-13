6a885b4e5c52c4d1636ed52c39b2c18d572e0159
sidguptajhs <siddhg@thoughtworks.com>
2014-01-13 14:38:34 +0530

    Add Visa Type Spec

---
+++ spec/models/visa_type_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe VisaType do
+  describe "visa_type factory" do
+    it "should create visa type"do
+      visa_type = FactoryGirl.create(:visa_type)
+      visa_type.should_not be_nil
+    end
+
+    it "should not create visa type with nill visa type value" do
+      expect{FactoryGirl.create(:visa_type, visa_type:nil)}.to raise_error
+
+    end
+
+  end
+end
